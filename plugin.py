4e46b85c4e93e8d6c56cdf1650d003625d728972
Tobias Rosenstock <tr+github@air-discordia.org>
2023-02-05 00:08:31 +0100

    Added support for Orion Smart DC/DC converters

--- plugin.py
+++ plugin.py
@@ -8,7 +8,7 @@ import sys
 from typing import Optional
 
 from bleak.backends.device import BLEDevice
-from victron_ble.devices import BatteryMonitor, SolarCharger
+from victron_ble.devices import BatteryMonitor, SolarCharger, DcDcConverter
 from victron_ble.exceptions import AdvertisementKeyMissingError, UnknownDeviceError
 from victron_ble.scanner import Scanner
 
@@ -52,6 +52,8 @@ class SignalKScanner(Scanner):
             self.log_battery(bl_device, configured_device, data, id_)
         elif isinstance(device, SolarCharger):
             self.log_solar_charger(bl_device, configured_device, data, id_)
+        elif isinstance(device, DcDcConverter):
+            self.log_dcdc_converter(bl_device, configured_device, data, id_)
         else:
             logger.debug("Unknown device", device)
 
@@ -135,6 +137,51 @@ class SignalKScanner(Scanner):
                         {
                             "path": f"electrical.solar.{id_}.chargingMode",
                             "value": data.get_charge_state().name.lower(),
+                        },
+                    ],
+                },
+            ],
+        }
+        logger.info(delta)
+        print(json.dumps(delta))
+        sys.stdout.flush()
+
+    def log_dcdc_converter(
+        self,
+        bl_device: BLEDevice,
+        cfg_device: ConfiguredDevice,
+        data: DcDcConverter,
+        id_: str,
+    ):
+        delta = {
+            "updates": [
+                {
+                    "source": {
+                        "label": "Victron",
+                        "type": "Bluetooth",
+                        "src": bl_device.address,
+                    },
+                    "timestamp": datetime.datetime.utcnow().isoformat() + "Z",
+                    "values": [
+                        {
+                            "path": f"electrical.converters.{id_}.chargingMode",
+                            "value": data.get_charge_state().name.lower(),
+                        },
+                        {
+                            "path": f"electrical.converters.{id_}.chargerError",
+                            "value": data.get_charger_error().name.lower(),
+                        },
+                        {
+                            "path": f"electrical.converters.{id_}.input.voltage",
+                            "value": data.get_input_voltage(),
+                        },
+                        {
+                            "path": f"electrical.converters.{id_}.output.voltage",
+                            "value": data.get_output_voltage(),
+                        },
+                        {
+                            "path": f"electrical.converters.{id_}.chargingMode.offReason",
+                            "value": data.get_off_reason().name.lower(),
                         },
                     ],
                 },
